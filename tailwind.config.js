fdf23645ba9edf8635591eacca5b5954ed59ae80
david.singh.3683@gmail.com <david.singh.3683@gmail.com>
2024-09-28 00:20:58 +0530

    layout update 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
-  content: ["./src/main/index.html"],
+  content: ["./main/index.html"],
   theme: {
     extend: {},
   },
